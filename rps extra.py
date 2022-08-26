0fd52da00a2f6951d700c364292ab2acf18618f6
Aakash Ayachit <110335913+AakashAyachit@users.noreply.github.com>
2022-08-26 13:30:27 +0530

    Add files via upload
    with added replay feature

---
+++ rps extra.py
@@ -0,0 +1,42 @@
+import random
+while True:
+    player = input("Choose Rock, Paper or Scissors!: ").lower()
+    computer = random.choice(["Rock", "Paper", "Scissor"]).lower()
+
+    print("Your virtual opponent has selected: ", computer)
+
+    start_game = input("Do you want to start the game? (Y or N): ")
+    if start_game.upper()[0] == 'Y':
+        gameplay = True
+    else:
+        gameplay = False
+
+    while gameplay:
+
+        if player == computer:
+            print('It is a tie!')
+
+        elif player == 'scissor' and computer == 'paper':
+            print('Scissor beats Paper, You win!')
+
+        elif player == 'rock' and computer == 'scissor':
+            print('Rock beats Scissor, You win!')
+
+        elif player == 'paper' and computer == 'rock':
+            print('Paper beats Rock, You win!')
+
+        else:
+            print("You lose")
+        break
+
+
+    def replay():
+        return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
+
+
+    if not replay():
+        break
+
+
+
+
